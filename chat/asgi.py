87db1cc3133496a3222e3638471607f452ace73a
Christian <28239264+WildDorito420@users.noreply.github.com>
2019-04-19 15:02:52 +1000

    Update asgi.py

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,5 +1,6 @@
 import os
-import channels
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
-channel_layer = channels.asgi.get_channel_layer()
+django.setup()
+application = get_default_application()
