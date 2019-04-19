226ca7872c899a84805cc4a8e01571ca8d67e99d
Christian <28239264+WildDorito420@users.noreply.github.com>
2019-04-19 15:07:26 +1000

    Update settings.py

--- chat/settings.py
+++ chat/settings.py
@@ -93,6 +93,8 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+ASGI_APPLICATION = "chat.routing.application"
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
